009203164afec078f37292e9a31683afd9803e84
Masud-Rana-1002 <masudrana19981002@gmail.com>
2024-12-26 18:55:45 +0300

    add

--- src/utils/hooks/useAxiosSecure.jsx
+++ src/utils/hooks/useAxiosSecure.jsx
@@ -6,35 +6,35 @@ import { useNavigate } from "react-router-dom";
 // Create axios instance with base URL and credentials
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_VOLUNTEER_MANAGEMENT_SERVER_URL,
-  withCredentials: true,  // Ensure cookies are sent with requests
+  withCredentials: true,  
 });
 
-// const useAxiosSecure = () => {
-//   const navigate = useNavigate();
-//   const { userLogout } = useContext(AuthContext);
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { userLogout } = useContext(AuthContext);
 
-//   useEffect(() => {
-//     const interceptor = axiosInstance.interceptors.response.use(
-//       (response) => {
-//         return response;
-//       },
-//       async (error) => {
-//         // Check for 401 or 403 errors
-//         if (error.response?.status === 401 || error.response?.status === 403) {
-//           await userLogout();  // Log the user out
-//           navigate("/login");  // Redirect to login page
-//         }
-//         return Promise.reject(error);
-//       }
-//     );
+  useEffect(() => {
+    const interceptor = axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        // Check for 401 or 403 errors
+        if (error.response?.status === 401 || error.response?.status === 403) {
+          await userLogout();  // Log the user out
+          navigate("/login");  // Redirect to login page
+        }
+        return Promise.reject(error);
+      }
+    );
 
-//     // Cleanup interceptor on unmount
-//     return () => {
-//       axiosInstance.interceptors.response.eject(interceptor);
-//     };
-//   }, [userLogout, navigate]); // Ensure the effect re-runs when these dependencies change
+    // Cleanup interceptor on unmount
+    return () => {
+      axiosInstance.interceptors.response.eject(interceptor);
+    };
+  }, [userLogout, navigate]); // Ensure the effect re-runs when these dependencies change
 
-//   return axiosInstance;
-// };
+  return axiosInstance;
+};
 
-// export default useAxiosSecure;
+export default useAxiosSecure;
